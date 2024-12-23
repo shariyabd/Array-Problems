e61338024611878e63f988f81eb39fb3699c9971
shariya5 <shariya.codeware@gmail.com>
2024-12-24 00:08:51 +0600

    first problem

---
+++ first.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main()
+{
+
+    // 3. * *Counting Pairs * *
+    //     -Given an array of integers,
+    //     print all unique pairs `(a, b)` such that `a != b`.- Example input : `[1, 2, 3]` - Example output :
+    //  ``` (1, 2)(1, 3)(2, 3)
+
+
+int arr[3] = {1, 2, 3};
+
+for (int i = 0; i < 3; i++)
+{
+    for (int j = i + 1; j < 3; j++)
+    {                                     
+        printf("%d%d\n", arr[i], arr[j]); 
+    }
+}
+
+        return 0;
+}
